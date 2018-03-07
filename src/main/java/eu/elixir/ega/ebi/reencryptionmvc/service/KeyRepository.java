27bccdc7d11aec79347fec2fd10498503b31dea4
Dmytro Titov <dmytro.titov@gmail.com>
2018-03-07 15:27:34 +0100

    Separate KeyRepository.

---
+++ src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/KeyRepository.java
@@ -0,0 +1,43 @@
+package eu.elixir.ega.ebi.reencryptionmvc.service;
+
+import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.bouncycastle.util.io.pem.PemReader;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.Iterator;
+
+@Repository
+public class KeyRepository {
+
+    public byte[] getAESPrivateKey(String sourceKey) throws IOException {
+        try (PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(sourceKey)))) {
+            return pemReader.readPemObject().getContent();
+        }
+    }
+
+    public PGPPublicKey getPGPPublicKey(String targetKey) throws IOException, PGPException {
+        InputStream in = new FileInputStream(new File(targetKey));
+        in = PGPUtil.getDecoderStream(in);
+        PGPPublicKeyRingCollection pgpPublicKeyRings = new PGPPublicKeyRingCollection(in, new BcKeyFingerprintCalculator());
+        PGPPublicKey pgpPublicKey = null;
+        Iterator keyRings = pgpPublicKeyRings.getKeyRings();
+        while (pgpPublicKey == null && keyRings.hasNext()) {
+            PGPPublicKeyRing kRing = (PGPPublicKeyRing) keyRings.next();
+            Iterator publicKeys = kRing.getPublicKeys();
+            while (publicKeys.hasNext()) {
+                PGPPublicKey key = (PGPPublicKey) publicKeys.next();
+                if (key.isEncryptionKey()) {
+                    pgpPublicKey = key;
+                    break;
+                }
+            }
+        }
+        if (pgpPublicKey == null) {
+            throw new IllegalArgumentException("Can't find encryption key in key ring.");
+        }
+        return pgpPublicKey;
+    }
+
+}
--- src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/ReencryptionService.java
+++ src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/ReencryptionService.java
@@ -7,9 +7,7 @@ import htsjdk.samtools.seekablestream.cipher.GPGCipherStream;
 import htsjdk.samtools.seekablestream.cipher.SeekableAESCipherStream;
 import org.apache.commons.io.input.BoundedInputStream;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openpgp.*;
-import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
-import org.bouncycastle.util.io.pem.PemReader;
+import org.bouncycastle.openpgp.PGPException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -18,16 +16,17 @@ import javax.annotation.PostConstruct;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Iterator;
 import java.util.Objects;
 
 @Service
 public class ReencryptionService {
 
     private ISeekableStreamFactory seekableStreamFactory;
+    private KeyRepository keyRepository;
 
     @PostConstruct
     private void init() {
@@ -43,50 +42,26 @@ public class ReencryptionService {
                                       long endCoordinate) throws IOException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidKeyException, InvalidKeySpecException, PGPException {
         SeekableStream seekableStream = seekableStreamFactory.getStreamFor(fileLocation);
         if (Format.AES.equals(sourceFormat)) {
-            seekableStream = new SeekableAESCipherStream(seekableStream, Objects.requireNonNull(getAESPrivateKey(sourceKey)));
+            seekableStream = new SeekableAESCipherStream(seekableStream, Objects.requireNonNull(keyRepository.getAESPrivateKey(sourceKey)));
         }
         seekableStream.seek(startCoordinate);
         InputStream inputStream = endCoordinate != 0 && endCoordinate > startCoordinate ?
                 new BoundedInputStream(seekableStream, endCoordinate - startCoordinate) :
                 seekableStream;
         if (Format.GPG.equals(targetFormat)) {
-            inputStream = new GPGCipherStream(inputStream, Objects.requireNonNull(getPGPPublicKey(targetKey)), StringUtils.getFilename(fileLocation));
+            inputStream = new GPGCipherStream(inputStream, Objects.requireNonNull(keyRepository.getPGPPublicKey(targetKey)), StringUtils.getFilename(fileLocation));
         }
         return inputStream;
-    }
-
-    private byte[] getAESPrivateKey(String sourceKey) throws IOException {
-        try (PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(sourceKey)))) {
-            return pemReader.readPemObject().getContent();
-        }
-    }
-
-    private PGPPublicKey getPGPPublicKey(String targetKey) throws IOException, PGPException {
-        InputStream in = new FileInputStream(new File(targetKey));
-        in = PGPUtil.getDecoderStream(in);
-        PGPPublicKeyRingCollection pgpPublicKeyRings = new PGPPublicKeyRingCollection(in, new BcKeyFingerprintCalculator());
-        PGPPublicKey pgpPublicKey = null;
-        Iterator keyRings = pgpPublicKeyRings.getKeyRings();
-        while (pgpPublicKey == null && keyRings.hasNext()) {
-            PGPPublicKeyRing kRing = (PGPPublicKeyRing) keyRings.next();
-            Iterator publicKeys = kRing.getPublicKeys();
-            while (publicKeys.hasNext()) {
-                PGPPublicKey key = (PGPPublicKey) publicKeys.next();
-                if (key.isEncryptionKey()) {
-                    pgpPublicKey = key;
-                    break;
-                }
-            }
-        }
-        if (pgpPublicKey == null) {
-            throw new IllegalArgumentException("Can't find encryption key in key ring.");
-        }
-        return pgpPublicKey;
     }
 
     @Autowired
     public void setSeekableStreamFactory(ISeekableStreamFactory seekableStreamFactory) {
         this.seekableStreamFactory = seekableStreamFactory;
+    }
+
+    @Autowired
+    public void setKeyRepository(KeyRepository keyRepository) {
+        this.keyRepository = keyRepository;
     }
 
 }
