b98ed77df849de4e3b7515055e1cd9b851788a09
Dmytro Titov <dmytro.titov@gmail.com>
2018-03-09 15:33:15 +0100

    First steps towards integration with LocalEGA keyserver.

--- src/main/java/eu/elixir/ega/ebi/reencryptionmvc/domain/Format.java
+++ src/main/java/eu/elixir/ega/ebi/reencryptionmvc/domain/Format.java
@@ -2,6 +2,6 @@ package eu.elixir.ega.ebi.reencryptionmvc.domain;
 
 public enum Format {
 
-    PLAIN, AES, GPG
+    PLAIN, AES, GPG_SYMMETRIC, GPG_ASYMMETRIC
 
 }
--- src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/KeyRepository.java
+++ src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/KeyRepository.java
@@ -1,19 +1,41 @@
 package eu.elixir.ega.ebi.reencryptionmvc.service;
 
+import com.google.gson.Gson;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
 import org.bouncycastle.util.io.pem.PemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.Iterator;
 
 @Repository
 public class KeyRepository {
 
-    public byte[] getRSAKeyById(String id) throws IOException {
-        // TODO: temporary implementation - treat "id" as file path
-        try (PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(id)))) {
+    private String keyServiceURL;
+    private RestTemplate restTemplate;
+
+    public byte[] getRSAKeyById(String id) throws IOException, DecoderException {
+        // TODO: bring that back after LocalEGA key server becomes able to register itself against Eureka
+        // ResponseEntity<Resource> responseEntity =
+        //        restTemplate.getForEntity(keyServiceURL + "/retrieve/rsa/rsa.key." + id, Resource.class);
+
+        // TODO: need to disable SSL verification in order to make it work; did not commit those files, because it's WA
+        // SSLUtilities.trustAllHostnames();
+        // SSLUtilities.trustAllHttpsCertificates();
+        HashMap response = new Gson().fromJson(IOUtils.toString(new URL(keyServiceURL + "/retrieve/rsa/rsa.key." + id).openStream(), Charset.defaultCharset()), HashMap.class);
+        String privateKey = String.valueOf(response.get("public")); // type here: will be replaced to "private"
+        byte[] privateKeyBytes = Hex.decodeHex(privateKey.toCharArray());
+        try (PemReader pemReader = new PemReader(new InputStreamReader(new ByteArrayInputStream(privateKeyBytes)))) {
             return pemReader.readPemObject().getContent();
         }
     }
@@ -40,6 +62,16 @@ public class KeyRepository {
             throw new IllegalArgumentException("Can't find encryption key in key ring.");
         }
         return pgpPublicKey;
+    }
+
+    @Value("${localega.keyserver.url:https://localhost:443}")
+    public void setKeyServiceURL(String keyServiceURL) {
+        this.keyServiceURL = keyServiceURL;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
 }
