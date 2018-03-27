bd92c099bd9c03dc9d1da833db6499215ef75e89
Dmytro Titov <dmytro.titov@gmail.com>
2018-03-27 15:06:26 +0200

    Fix Spring profiles.

--- src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/internal/LocalEgaServiceImpl.java
+++ src/main/java/eu/elixir/ega/ebi/reencryptionmvc/service/internal/LocalEgaServiceImpl.java
@@ -15,7 +15,6 @@
  */
 package eu.elixir.ega.ebi.reencryptionmvc.service.internal;
 
-import com.google.common.io.ByteStreams;
 import eu.elixir.ega.ebi.reencryptionmvc.domain.Format;
 import eu.elixir.ega.ebi.reencryptionmvc.service.KeyService;
 import eu.elixir.ega.ebi.reencryptionmvc.service.ResService;
@@ -24,27 +23,8 @@ import htsjdk.samtools.seekablestream.SeekableStream;
 import htsjdk.samtools.seekablestream.cipher.GPGAsymmetricCipherStream;
 import htsjdk.samtools.seekablestream.cipher.GPGSymmetricCipherStream;
 import htsjdk.samtools.seekablestream.cipher.SeekableAESCipherStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.Security;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.codec.DecoderException;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.BoundedInputStream;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.PGPException;
@@ -56,8 +36,20 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
  * @author asenf
  */
 @Service
@@ -70,35 +62,35 @@ public class LocalEgaServiceImpl implements ResService {
 
     @Autowired
     private KeyService keyService;
-    
+
     // This should be in DataEdge
     //private static final String CONTENT_DISPOSITION_PREFIX = "attachment; filename=";
-    
+
     @PostConstruct
     private void init() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
     @Override
-    public void transfer(String sourceFormat, 
-                         String sourceKey, 
-                         String destinationFormat, 
-                         String destinationKey, 
-                         String destinationIV, 
-                         String fileLocation, 
-                         long startCoordinate, 
-                         long endCoordinate, 
-                         long fileSize, 
-                         String httpAuth, 
-                         String id, 
-                         HttpServletRequest request, 
+    public void transfer(String sourceFormat,
+                         String sourceKey,
+                         String destinationFormat,
+                         String destinationKey,
+                         String destinationIV,
+                         String fileLocation,
+                         long startCoordinate,
+                         long endCoordinate,
+                         long fileSize,
+                         String httpAuth,
+                         String id,
+                         HttpServletRequest request,
                          HttpServletResponse response) {
 
         InputStream inputStream = null;
         OutputStream outputStream = null;
         try {
             outputStream = response.getOutputStream();
-            
+
             inputStream = getInputStream(Format.valueOf(sourceFormat.toUpperCase()),
                     sourceKey,
                     Format.valueOf(destinationFormat.toUpperCase()),
@@ -106,30 +98,20 @@ public class LocalEgaServiceImpl implements ResService {
                     fileLocation,
                     startCoordinate,
                     endCoordinate);
-        } catch (IOException | NoSuchPaddingException | InvalidAlgorithmParameterException | NoSuchAlgorithmException | IllegalBlockSizeException | BadPaddingException | NoSuchProviderException | InvalidKeyException | InvalidKeySpecException | PGPException | DecoderException ex) {
-            Logger.getLogger(LocalEgaServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            Logger.getLogger(LocalEgaServiceImpl.class.getName()).log(Level.SEVERE, null, e);
         }
-                
+
         response.setStatus(200);
         response.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        // This should be in DataEdge!
-        //response.addHeader(HttpHeaders.CONTENT_DISPOSITION, CONTENT_DISPOSITION_PREFIX + StringUtils.getFilename(fileLocation));
-
         try {
-            // Copy
-            long bytes = ByteStreams.copy(inputStream, outputStream);
-            
-        } catch (IOException ex) {
-            Logger.getLogger(LocalEgaServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                outputStream.close();
-                inputStream.close();
-            } catch (IOException ex) {}
+            IOUtils.copyLarge(inputStream, outputStream);
+        } catch (IOException e) {
+            Logger.getLogger(LocalEgaServiceImpl.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
-    /* 
+    /*
      * Recommendation: Split decrypting and encrypting into separate input and output streams
      */
     public InputStream getInputStream(Format sourceFormat,
@@ -138,29 +120,30 @@ public class LocalEgaServiceImpl implements ResService {
                                       String targetKeyId,
                                       String fileLocation,
                                       long startCoordinate,
-                                      long endCoordinate) throws IOException, 
-                                                                 NoSuchPaddingException, 
-                                                                 InvalidAlgorithmParameterException, 
-                                                                 NoSuchAlgorithmException, 
-                                                                 IllegalBlockSizeException, 
-                                                                 BadPaddingException, 
-                                                                 NoSuchProviderException, 
-                                                                 InvalidKeyException, 
-                                                                 InvalidKeySpecException, 
-                                                                 PGPException, 
-                                                                 DecoderException {
+                                      long endCoordinate) throws IOException,
+            NoSuchPaddingException,
+            InvalidAlgorithmParameterException,
+            NoSuchAlgorithmException,
+            IllegalBlockSizeException,
+            BadPaddingException,
+            NoSuchProviderException,
+            InvalidKeyException,
+            InvalidKeySpecException,
+            PGPException,
+            DecoderException {
 
         SeekableStream seekableStream = seekableStreamFactory.getStreamFor(fileLocation);
-        if (Format.AES.equals(sourceFormat)) {
-            if (StringUtils.isEmpty(sourceKeyId)) {
-                seekableStream.seek(0);
-                InputStreamReader inputStreamReader = new InputStreamReader(seekableStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String header = bufferedReader.readLine();
-                sourceKeyId = header.split("\\|")[0];
-            }
-            seekableStream = new SeekableAESCipherStream(seekableStream, Objects.requireNonNull(keyService.getRSAKeyById(sourceKeyId)));
+        // TODO: WA for DataEdge that is not capable of passing source format here...
+//        if (Format.AES.equals(sourceFormat)) {
+        if (StringUtils.isEmpty(sourceKeyId)) {
+            seekableStream.seek(0);
+            InputStreamReader inputStreamReader = new InputStreamReader(seekableStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String header = bufferedReader.readLine();
+            sourceKeyId = header.split("\\|")[0];
         }
+        seekableStream = new SeekableAESCipherStream(seekableStream, Objects.requireNonNull(keyService.getRSAKeyById(sourceKeyId)));
+//        }
         seekableStream.seek(startCoordinate);
         InputStream inputStream = endCoordinate != 0 && endCoordinate > startCoordinate ?
                 new BoundedInputStream(seekableStream, endCoordinate - startCoordinate) :
@@ -172,5 +155,5 @@ public class LocalEgaServiceImpl implements ResService {
         }
         return inputStream;
     }
-    
+
 }
